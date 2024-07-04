4616948ca5f4ab9241fb3f3f2e16b43180edba01
amitPattanaik987 <amitpattanaik987@gmail.com>
2024-07-04 11:45:28 +0530

    Google gemini

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css"
+import Sidebar from './components/sidebar/Sidebar'
+import Main from './components/main/Main'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Sidebar></Sidebar>
+      <Main></Main>
+    </div>
+  )
+}
+
+export default App
